fa3a5c62d8c50b974b3fa2cae9c87d99e2f98817
Kaylyn Cardella <kaylyncardella@gmail.com>
2021-08-10 23:54:37 -0700

    Add expandable cardview shell for treatment plan feature

--- app/src/main/java/com/example/lest/TreatmentPlanHomeActivity.java
+++ app/src/main/java/com/example/lest/TreatmentPlanHomeActivity.java
@@ -1,14 +1,42 @@
 package com.example.lest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class TreatmentPlanHomeActivity extends AppCompatActivity {
+
+    ImageButton arrow;
+    LinearLayout hiddenView;
+    CardView cardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_treatment_plan_home);
+
+        cardView = findViewById(R.id.base_cardview);
+        arrow = findViewById(R.id.arrow_button);
+        hiddenView = findViewById(R.id.hidden_view);
+
+        arrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+                public void onClick(View view) {
+                if (hiddenView.getVisibility() == View.VISIBLE) {
+                    TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
+                }
+                else {
+                    TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
+                    hiddenView.setVisibility(View.VISIBLE);
+                    arrow.setImageResource(R.drawable.ic_baseline_expand_less_24);
+                }
+            }
+        });
     }
 }
